2de6c02fa93c5584190db1f1805630e6334ccc00
Charl <info@charlroux.co.za>
2020-12-14 20:48:49 +0200

    Navbar styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import React,{useState} from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar';
 function App() {
-  return (
-    <Navbar/>
-  );
+  const [menuSate, setMenuState] = useState({isMenuOpen: false});
+  const updateMenuState = () => {
+      menuSate === true?  setMenuState(false):setMenuState(true)
+  }
+  return <Navbar navToggle={updateMenuState} isMenuOpen={menuSate} />;
 }
 
 export default App;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,38 +1,72 @@
-import React from 'react'
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Facebook from '../svg/facebook.svg';
+import Facebook from "../svg/facebook.svg";
 import Linkedin from "../svg/linkedin.svg";
 import Twitter from "../svg/twitter.svg";
-import './style.css';
-const navbar = () => {
-    return (
-      <div className="navbar-container">
-        <Navbar
-          collapseOnSelect
-          expand="lg"
-          sticky="top"
-          className="light-navbar"
-        >
-          <div className="brand-left mr-auto">
-            <div className="d-flex">
-              <div className="mr-2">
-                <img src={Facebook} />
-              </div>
-              <div className="mr-2">
-                <img src={Linkedin} />
-              </div>
-              <div className="mr-2">
-                <img src={Twitter} />
+import Hamburger from 'react-hamburgers';
+import "./style.css";
+const MainNavbar = (props) => {
+  return (
+    <div className="navbar-container">
+      <Navbar expand="lg" sticky="top" className="light-navbar">
+        <div className="brand-left">
+          <div className="d-flex">
+            <div className="mr-2">
+              <a href="https://www.facebook.com/charl.roux.52/">
+                <img src={Facebook} alt="Facebook icone" />
+              </a>
+            </div>
+            <div className="mr-2">
+              <a href="https://www.linkedin.com/in/charl-roux-50b124122/">
+                <img src={Linkedin} alt="LinkedIn icone" />
+              </a>
+            </div>
+            <div className="mr-2">
+              <a href="https://twitter.com/charlroux641">
+                <img src={Twitter} alt="Twitter icone" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <Navbar.Collapse className="justify-content-center">
+          <Navbar.Text>Charl Roux</Navbar.Text>
+        </Navbar.Collapse>
+        <div className="toggleIcon" onClick={props.navToggle}>
+          <div
+            id="nav-icon1"
+            className={props.isMenuOpen === true ? "open" : ""}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </Navbar>
+      <div
+        className={`collapseDiv ${
+          props.isMenuOpen === true ? "open scale-up-ver-bottom" : "closed"
+        }`}
+      >
+        <div className="collapseContainer">
+          <div className="container">
+            <div className="row py-5 mt-5">
+              <div className="col text-center">
+                <div className="link pb-5" onClick={props.navToggle}>
+                  Home
+                </div>
+                <div className="link pb-5" onClick={props.navToggle}>
+                  About
+                </div>
+                <div className="link pb-5" onClick={props.navToggle}>
+                  Contact
+                </div>
               </div>
             </div>
           </div>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>Charl Roux</Navbar.Text>
-          </Navbar.Collapse>
-        </Navbar>
+        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default navbar;
+export default MainNavbar;
